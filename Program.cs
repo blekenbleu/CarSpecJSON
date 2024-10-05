e474ce9994979c5bdc327fa3317d269b8fcd79eb
Steven Carmichael <blekenbleu@gmail.com>
2024-10-05 19:39:21 -0400

    credible output

--- Program.cs
+++ Program.cs
@@ -1,10 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
-using System;
-using Newtonsoft.Json;
+﻿using Newtonsoft.Json;
 using blekenbleu;
-using System.Net;
-using System.Xml.Linq;
-using System.Collections.Generic;
 
 namespace CarSpecJSON
 {
@@ -13,14 +8,18 @@ namespace CarSpecJSON
 
 		static void Main(string[] args)
 		{
-			string version = "version 1.3 ";
+			string version = "version 1.4 ";
 			string bname = "blekenbleu";
 			string pname = "Haptics";
 			string myfile = $"D:/my/SimHub/PluginsData/{pname}.{Environment.UserName}.json";
+			string mysource = "R:/Temp/New.cs";
 
 			if (args.Length > 1 )
+			{
 				myfile = args[1];
-
+				if (2 < args.Length)
+				mysource = args[2];
+			}
 			if (File.Exists(myfile))
 			{
 				string text = File.ReadAllText(myfile);
@@ -32,9 +31,9 @@ namespace CarSpecJSON
 					if (null != json)
 					{
 						Console.WriteLine(version + bname + ".Main():  JsonConvert non-null " + myfile);
-						Dictionary<string, List<CarSpec>> atlas = json!;	// force non-null
-                        Console.WriteLine(bname + $".Main({myfile}): {P.JtoSource(json!).Length} source length");
-
+                        Console.WriteLine(bname + $".Main({myfile}): "
+										// force non-null
+							+ $"{P.JtoSource(json!, mysource).Length} source length");
                     }
 				} else Console.WriteLine(bname + ".Main():  null " + myfile);
 			}
@@ -43,34 +42,44 @@ namespace CarSpecJSON
 
         readonly string[] sname = ["name", "category", "config", "order", "loc", "drive"];
         readonly string[] uname = ["idlerpm", "redline", "maxrpm", "cyl", "hp", "ehp", "cc", "nm"];
-		string source = "AtlasDict = new()\n {\n\t{\n\t\t";
+		string source = "AtlasDict = new()\n{";
 		void Sadd(int index, string? value)
 		{
 			if (null == value || 0 == value.Length)
 				return;
-			source += $",\n\t\t\t\t{sname[index]} = \"{value}\"";
+			source += $",\n\t\t\t{sname[index]} = \"{value}\"";
 		}
 
         void Uadd(int index, ushort? value)
         {
 			if (null == value || 0 == value)
 				return;
-            source += $",\n\t\t\t\t{uname[index]} = {value}";
+            source += $",\n\t\t\t{uname[index]} = {value}";
         }
 
-		string JtoSource(Dictionary<string, List<CarSpec>> atlas)
+		string JtoSource(Dictionary<string, List<CarSpec>> atlas, string file)
 		{
+			bool firstgame = true;
 			foreach (var game in atlas)
 			{
-				source += $"\"{game.Key}\", new() {{\n ";
+				if (!firstgame)
+					source += "\n\t\t}\n\t},";
+                firstgame = false;
+				source += $"\n\t\"{game.Key}\", new() {{\n ";
+				bool firstcar = true;
                 foreach (var car in game.Value)
 				{
-                    source += $"\t\t\t new() {{\n\t\t\t\tid = \"{car.id}\"";
+					if (!firstcar)
+						source += "\n\t\t},\n";
+                    source += $"\t\tnew() {{\n\t\t\tid = \"{car.id}\"";
 					Sadd(0, car.name);
 					Uadd(0, car.idlerpm);
-				}
-				source += "\n\t\t\t},\n";
+                    firstcar = false;
+                }
+//				source += "\n\t\t}";
 			}
+			source += "\n\t\t}\n\t}\n}";
+			File.WriteAllText(file, source);
 			return source;
 		}
 	}
