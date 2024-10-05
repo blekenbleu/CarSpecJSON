a6885ea2d6e437cd6f79d65e68533f924ba96c5f
Steven Carmichael <blekenbleu@gmail.com>
2024-10-04 20:14:27 -0400

    Newtonsoft

---
+++ CarSpec.cs
@@ -0,0 +1,25 @@
+﻿namespace blekenbleu
+{
+    public class CarSpec
+	{
+		public string? game;
+		public string? name;
+		public required string id;
+		public string? config;
+		public ushort? cyl;
+		public string? loc;
+		public string? drive;
+		public ushort? hp;
+		public ushort? ehp;
+		public ushort? cc;
+		public ushort? nm;
+		public ushort? redline;
+		public ushort? maxrpm;
+		public ushort? idlerpm;							// CarSpec element
+		public string? order;							// firing order added 19 Jun 2024
+		public string? category;
+		public string? notes;
+		public string? defaults;
+		public string? properties;
+	}   // class CarSpec
+}
---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using Newtonsoft.Json;
+using blekenbleu;
+
+namespace CarSpecJSON
+{
+	internal class Program
+	{
+		
+		static void Main(string[] args)
+		{
+			string version = "version 1.2 ";
+			string bname = "blekenbleu";
+            string pname = "Haptics";
+            string myfile = $"D:/my/SimHub/PluginsData/{pname}.{Environment.UserName}.json";
+            if (File.Exists(myfile))
+			{
+				string text = File.ReadAllText(myfile);
+				if (null != text)
+				{
+					Dictionary<string, List<CarSpec>>? json =
+						JsonConvert.DeserializeObject<Dictionary<string, List<CarSpec>>>(text);
+					if (null != json)
+						Console.WriteLine(version + bname + ".Main():  JsonConvert non-null " + myfile);
+				} else Console.WriteLine(bname + ".Main():  null " + myfile);
+			}
+			else Console.WriteLine(bname + ".Main():  " + myfile + " not found"); 
+		}
+	}
+}
